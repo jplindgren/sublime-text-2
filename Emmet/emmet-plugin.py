b598e331985902a0991d3660c40ee6e92131dc4f
Nando Vieira <fnando.vieira@gmail.com>
2013-04-15 19:15:24 -0300

    Updates.

--- Emmet/emmet-plugin.py
+++ Emmet/emmet-plugin.py
@@ -270,7 +270,12 @@ def should_handle_tab_key(syntax=None):
 		return True
 
 	abbr = ctx.js().locals.pyExtractAbbreviation()
-	if not re.match(r'^[\w\:%]+$', abbr):
+
+	disabled_snippets = settings.get('disabled_single_snippets', '').split()
+	if disabled_snippets and abbr in disabled_snippets:
+		return False
+
+	if not re.match(r'^[\w\-\:%]+$', abbr):
 		# it's a complex expression
 		return True
 
